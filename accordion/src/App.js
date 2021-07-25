decf6e3890c7451d58658a67c1387305c565ea06
Serine-t <serinetovmasyan98@gmail.com>
2021-07-25 17:07:22 +0400

    accordion project

---
+++ accordion/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import data from './data';
+import Question from './Question';
+
+function App() {
+  return (
+    <main>
+      <div className='container'>
+        <h3>Questions and answers</h3>    
+        <section className='info'>
+          {data.map(question=>{
+            return <Question key={question.id} {...question}/>
+          })}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App;
