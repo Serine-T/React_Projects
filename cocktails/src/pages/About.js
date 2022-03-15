33044791b473ae0077330b374c1886ebf9a1cd1a
Serine-t <serinetovmasyan98@gmail.com>
2022-03-15 17:54:02 +0400

    cocktail project

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptatibus fuga, sequi excepturi possimus architecto placeat, minus aut explicabo laboriosam porro, consequatur vel cum. Quis porro quisquam quia illum at quaerat hic est aut pariatur repellendus atque, eius soluta. Doloremque!
+      </p>
+    </section>
+  )
+}
+
+export default About
